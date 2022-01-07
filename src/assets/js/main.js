116b291980c2f343f92029f5b783b47d75837aa5
office PC <teddywijaya93@gmail.com>
2022-01-07 12:04:09 +0700

    config main.js and path

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,54 @@
 //main.js file
+
+//main.js file
+const app = (() => {
+
+	// var here
+
+	// let name = '..'
+
+	// -----------------
+
+
+
+	// Auto Run Type Here
+
+	function runItHere() {
+		tes()
+	}
+
+	// -----------------
+
+
+	
+	// All Function Here
+
+	function tes() {
+		console.log("Done!");
+	}
+
+    function printLog() {
+        console.log('Call this function inline directly');
+    }
+
+	// -----------------
+
+
+	
+	// Auto Run Call Here
+
+	runItHere();
+
+	// -----------------
+
+
+	
+	// Callback Function Here Ex: app.printLog()
+
+	return {
+        printLog
+    }
+	
+	// -----------------
+
+})()
