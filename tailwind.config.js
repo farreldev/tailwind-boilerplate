5dedf1bd30d3bcbca856117dc325a3a2aac3ed80
office PC <teddywijaya93@gmail.com>
2022-01-07 11:56:10 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      '2xl': {'min': '1280px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '575px'}
+    },
     extend: {},
   },
   plugins: [],
